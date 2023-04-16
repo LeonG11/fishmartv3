d735f79f9c645e08cdea7b5435d6ef832bd274ef
LeonG11 <kkacanov198@gmail.com>
2023-04-16 07:44:00 +0300

    Make fishfish page

--- src/Components/FishFish.jsx
+++ src/Components/FishFish.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import Footer from "./Footer";
 
 export default function FishFish() {
   return (
-    <div className="fishfish">
-      <div className="fish__head">FISH FISH</div>
-      <div className="fish__container">
-        <div>
-          Мы «FISH FISH» занимаемся доставкой свежих морепродуктов и рыбы на
-          любой вкус. Всегда только свежие продукты в охлажденном состоянии,
-          герметичной упаковке. Наши курьеры привозят рыбу и морепродукты в
-          переносных холодильных пакетах или камерах. Качество продукции для нас
-          превыше всего!
+    <>
+      <div className="fishfish">
+        <div className="fish__head">FISH FISH</div>
+        <div className="fish__container">
+          <p>
+            Мы «FISH FISH» занимаемся доставкой свежих морепродуктов и рыбы на
+            любой вкус. Всегда только свежие продукты в охлажденном состоянии,
+            герметичной упаковке. Наши курьеры привозят рыбу и морепродукты в
+            переносных холодильных пакетах или камерах. Качество продукции для
+            нас превыше всего!
+          </p>
+          <div className="fishfish__img"></div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import LogoSvg from "./LogoSvg";
+import LinkHeader from "./LinkHeader";
+import Dot from "./Dot";
+
+export default function Footer() {
+  return (
+    <header className="footer" style={{ backgroudColor: "#1C3751C7" }}>
+      <LogoSvg />
+      <div className="middle">
+        <LinkHeader path="/fishfish" text="FISH FISH" />
+        <Dot />
+        <LinkHeader path="/menu" text="АССОРТИМЕНТ" />
+        <Dot />
+        <LinkHeader path="/about" text="КОНТАКТЫ" />
+      </div>
+      <a className="last telephone" href="tel:8800000000">
+        +7 (800) 000-00-00
+      </a>
+    </header>
+  );
+}
